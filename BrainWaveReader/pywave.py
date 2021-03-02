eacf5372533a1ebdba4fc0ea834327233cf2e5ed
Morgan Kitto <morgan@Morgans-MacBook.local>
2021-03-02 13:09:28 +0000

    functioning object oriented EEG code

---
+++ BrainWaveReader/pywave.py
@@ -0,0 +1,60 @@
+import socket
+import json
+
+
+class pyWave:
+
+    configStr = "{ 'enableRawOutput': 'enableRawOutput', 'format': 'Json'}"
+    configByte = configStr.encode()
+
+    def __init__(self, _host, _port):
+        self.host = _host
+        self.port = _port
+
+    def connect(self):
+        # This is a standard connection for an Internet socket
+        # AF.INET is how you declare an internet socket
+
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect to the Thinkgear Connector
+        client.connect((self.host, self.port))
+        # calling this client to implement elsewhere
+
+        client.send(self.configByte)
+
+        return client
+
+    def readData(self, _client):
+
+        # This loop just waits for messages via the socket
+        # while True:
+        # The connection could break for lots of reasons to wrapping this in a try / catch
+        try:
+            # When a message is received write it to the data var
+            #  uses a buffer to transfer packets, buffer size is 2^10
+            data = _client.recv(1024)
+
+            data_json = json.loads(data)
+            eSenseData = data_json["eSense"]
+            attention = eSenseData["attention"]
+            return attention
+
+        # loop for
+        except Exception as e:
+            # print(str(e))
+            if str(e) == "'eSense'":
+                return None
+            else:
+                print(e)
+                return None
+
+
+if __name__ == '__main__':
+
+    pywave = pyWave("localhost", 13854)
+    client = pywave.connect()
+
+    print("Waiting for data")
+    while True:
+
+        print(pywave.readData(client))
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Mind-Control-Car
 Software for a car that is controlled by brain and eye movements.
+
+when running the EEG, for the most reliable results activate stac in this sequence.
+1. Turn on thinkgear connector.
+2. Turn on program.
+3. Turn on EEG.
+
+while this may not be the most effective method of starting the program, I found this sequence was the best.
